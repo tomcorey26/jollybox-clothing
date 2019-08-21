1e3ddbc3cf8bc5ce4d4d6590fa1be8bec4db671e
tomcorey26 <tomcorey26@gmail.com>
2019-08-20 21:16:07 -0400

    collection preview and collection item componets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = props => (
   <div>
@@ -17,7 +18,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
