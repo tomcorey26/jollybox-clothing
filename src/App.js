cdd545a6137b2fc8d2484e92e080d9b49184ec76
tomcorey26 <tomcorey26@gmail.com>
2019-08-21 11:54:08 -0400

    header component added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
-const HatsPage = props => (
-  <div>
-    <button onClick={() => props.history.push("/topics")} />
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
